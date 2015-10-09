9b03c4a952e74b1a1ba8b704050abd994b9e5820
Ann Marie Reinhold <AMReinhold@users.noreply.github.com>
2015-10-09 12:07:53 -0600

    first commit of iterateGangsta function

--- R/lpAPI.R
+++ R/lpAPI.R
@@ -5,24 +5,24 @@ readGangsta.lp = function(lpFile = file.choose()){
 }
 
 ### This function allows the user to access the columns of the lp object.
-getModelCol.lp = function(gangsta.lp, colnum) {
-  sapply(1:dim(lp.model)[1], function(i) get.mat(lp.model, i, colnum))
+getModelCol.lp = function(lpObject, colnum) {
+  sapply(1:dim(lpObject)[1], function(i) get.mat(lpObject, i, colnum))
 }
 
 ###  This function allows the user to view the values of the lp object.
-showMatrixVals.lp = function(varName, matrix = lpMatrix, lpObject = gangsta.lp) {
+showMatrixVals.lp = function(varName, matrix = lpMatrix, lpObject) {
   varCol = which(colnames(matrix) == varName)
   varRows = which(getModelCol(lpObject, varCol) != 0)
   matrix[varRows, varCol]
 }
 
 ###  This block of code builds a matrix of decision variables and constraints
-viewMatrix.lp = function(gangsta.lp){
-  constraintVarNames = dimnames(gangsta.lp)[[1]]
-  decisionVarNames = dimnames(gangsta.lp)[[2]]
+viewMatrix.lp = function(lpObject){
+  constraintVarNames = dimnames(lpObject)[[1]]
+  decisionVarNames = dimnames(lpObject)[[2]]
   nvars = length(decisionVarNames)
   nconstraints = length(constraintVarNames)
-  lpMatrix = matrix(mapply(function(i,j) lpSolveAPI::get.mat(gangsta.lp, i, j),
+  lpMatrix = matrix(mapply(function(i,j) lpSolveAPI::get.mat(lpObject, i, j),
                            i=1:nconstraints, j=rep(1:nvars, each=nconstraints)),
                     nrow = nconstraints)
   colnames(lpMatrix) = decisionVarNames
@@ -31,9 +31,9 @@ viewMatrix.lp = function(gangsta.lp){
 }
 
 ### View output in data frame
-solvedDataFrame.lp = function(gangsta.lp, simple = TRUE) {
-  variableOutput = lpSolveAPI::get.variables(gangsta.lp)[order(dimnames(gangsta.lp)[[2]])]
-  names(variableOutput) = dimnames(gangsta.lp)[[2]][order(dimnames(gangsta.lp)[[2]])]
+solvedDataFrame.lp = function(lpObject, simple = TRUE) {
+  variableOutput = lpSolveAPI::get.variables(lpObject)[order(dimnames(lpObject)[[2]])]
+  names(variableOutput) = dimnames(lpObject)[[2]][order(dimnames(lpObject)[[2]])]
   variableOutput = data.frame(variableOutput)
   if(simple == FALSE){
     return(variableOutput)
@@ -44,21 +44,112 @@ solvedDataFrame.lp = function(gangsta.lp, simple = TRUE) {
   }
 }
 
-
-poolDifs = function(gangstaObjects, lp, simple = T) {
+### Make a data frame with three columns: start values, end values, and changes in values
+poolDifs = function(gangstaObjects, lpObject, simple = T) {
   pools = subsetGangstas(gangstaObjects, "class", gangstaClassName("pool"))
   poolNames = getGangstaAttribute(pools, gangstaAttributeName("name"))
+  elementName = getGangstaAttribute(pools, gangstaAttributeName("element"))
+  elementIdx = order(elementName)
 
   initPoolNames = paste0(poolNames, ".", gangstaVarName("startSuffix"))
   finalPoolNames = paste0(poolNames, ".", gangstaVarName("endSuffix"))
 
-  df.lp = solvedDataFrame.lp(lp, simple = F)
+  df.lp = solvedDataFrame.lp(lpObject, simple = F)
 
   startVals = df.lp[initPoolNames,]
   endVals = df.lp[finalPoolNames,]
   allDF = data.frame(change = endVals - startVals, initial = startVals, final = endVals)
   row.names(allDF) = poolNames
+
+  allDF = allDF[elementIdx, ]
+
   if(simple) allDF = subset(allDF, (allDF$initial != 0) | (allDF$final != 0))
 #  col.names(appDF) = c("initial", "final", "change")
   return(allDF)
+}
+
+### Make a vector of mass transfers between pools
+massTransfers = function(gangstaObjects, lpObject, simple = FALSE, byProcess = FALSE) {
+
+  ### get classes, names, attributes, and gangstaObjectss that are used later in this function
+  metabolicClassName =  gangstaClassName("metab")
+  transClassName =      gangstaClassName("trans")
+
+  nameAttrName =      gangstaAttributeName("name")
+  procNameAttrName =  gangstaAttributeName("procName")
+
+  transSuffix = gangstaVarName("transSuffix")
+
+  metabolics =      subsetGangstas(gangstaObjects, "class", metabolicClassName)
+  transformations = subsetGangstas(gangstaObjects, "class", transClassName)
+
+  metabolicNames = getGangstaAttribute(metabolics, nameAttrName)
+
+  ### make metabolic transformation variable names from gangstaObjects
+  metabolicTransformations = lapply(metabolicNames, subsetGangstas, gangstaObjects = transformations, attributeName = procNameAttrName)
+  metabolicTransformations = unlist(metabolicTransformations, recursive = FALSE)
+  makeTransformationMassTransVars = function(transformationNames) {
+    return(paste0(transformationNames, ".", transSuffix))
+  }
+  metabolicTransformationNames = getGangstaAttribute(metabolicTransformations, nameAttrName)
+  metabolicTransformationMassTransVars = makeTransformationMassTransVars(metabolicTransformationNames)
+
+  ### make two named vectors: one of the from pools and one of the to pools
+  massTransferFroms = unlist(lapply(metabolicTransformations, "[[", "from") )
+  massTransferTos = unlist(lapply(metabolicTransformations, "[[", "to") )
+  massTransferFromsTosNames = paste(unlist(lapply(metabolicTransformations, "[[", nameAttrName)), transSuffix, sep = ".")
+  names(massTransferFroms) = names(massTransferTos) = massTransferFromsTosNames
+
+  ### make a data frame of lpsolve results that will be subsetted using the metabolicTransformationMassTransVars from gangsta
+  massTransferDF = solvedDataFrame.lp(lpObject, simple = FALSE)
+  subsettingCriteria = row.names(massTransferDF) %in% metabolicTransformationMassTransVars
+  massTransferNames = row.names(massTransferDF)[subsettingCriteria]
+  if(length(massTransferFromsTosNames) != length(massTransferNames))
+    stop("The number of mass transfer variables generated from your gangsta list doesn't match the number of mass transfer variable names in your lpSolve file.")
+  if(any(massTransferFromsTosNames %in% massTransferNames) == FALSE)
+    stop("Your names of mass transfers in your gangsta list doesn't match your mass transfer variable names in your lpSolve file.")
+
+  ### ensure that the order of the froms and tos matches that of the transfers in the dataframe
+  massTransferFroms = massTransferFroms[match(massTransferNames, massTransferFromsTosNames)]
+  massTransferTos = massTransferTos[match(massTransferNames, massTransferFromsTosNames)]
+
+  ### subset the data frame
+  massTransfers = massTransferDF[subsettingCriteria,]
+  massTransfers = data.frame(massTransferFroms, massTransferTos, massTransfers)
+  names(massTransfers) = c("fromPool", "toPool", "massTransfered")
+  row.names(massTransfers) = massTransferNames
+  if(byProcess == TRUE) {
+    return(massTransfers)
+  } else {
+    massTransfers = plyr::ddply(massTransfers, c("fromPool", "toPool"), plyr::summarise,
+                massTransfered = sum(massTransfered)
+                )
+    return(massTransfers)
+  }
+}
+
+### Make plots of multiple lp models
+#############  THIS NEEDS UPDATING WHEN YOU FIGURE OUT HOW TO MAKE SEQUENTIAL UPDATES TO COMPOUNDS
+massTransfersPlot = function(gangstaObjects, lpObjects, lineMult = 1, dotMult = 1){
+
+  nPlots = (2*length(lpObjects)) - 1
+  layoutMatrix = matrix(nrow = 1, ncol = nPlots, data = 1:nPlots)
+  layoutWidths = c(2, rep(c(1,2), length(lpObjects)-1))
+  layout(layoutMatrix, layoutWidths)
+  layoutMarginsFirstPlot = c(3,12,1,0)
+  layoutMarginsSubsequentPlot = c(3,0,1,0.08)
+  layoutMarginsLastPlot = c(3,0,1,12)
+  par(oma = c(1,7,1,7))
+
+  lapply(lpObjects, function(lpObject) {
+    solve(lpObject)
+    pd <<- poolDifs(gangstaObjects, lpObject, simple = FALSE, byProces = FALSE)
+    mt <<- massTransfers(gangstaObjects, lpObject, simple = FALSE, byProcess = FALSE)
+    poolOrder <<- abs(seq(-nrow(pd), -1, 1))
+    xPosnInit <<- (lpObject %in% lpObjects)
+  },
+  gangstaObjects
+  )
+
+
 }
--- R/test.R
+++ R/test.R
@@ -1,45 +1,27 @@
-# runTest = function() {
-#   testRun = runGangsta(file.choose())
-# }
 
-testList = function() {
-  return(
-    list(
-      name = "Aerobic",
-      energyTerm = 4.37-04,
-      fromCompoundNames = list(C = c("DOM", "."), N = c("DOM", "."), O = "O2"),
-      toCompoundNames = list(C = "CO2", N = "NH4", O = "Ox"),
-      organismName = c("Het", "Aut", "Met"),
-      molarTerms = list(C = 1, N = NA, O = 2),
-      limitToInitMols = c(F, T, T),
-      processSuffix = c("ofDOM", "ofHet")
-    )
-  )
-}
-
-gangstaTest = function() {
-
+gangstaCompounds = function() {
   compoundParams = list(
-    list(compoundName = "Het", molarRatios = c(C=1, N=16/106), initialMols = 0.5, respirationRate = -2.83E-6),
-    list(compoundName = "Aut", molarRatios = c(C=1, N=16/106), initialMols = 0.5, respirationRate = -2.83E-6),
-    list(compoundName = "Met", molarRatios = c(C=1, N=16/106), initialMols = 0.5, respirationRate = -2.83E-6),
+    list(compoundName = "Het", molarRatios = c(C=1, N=16/106), initialMols = 0, respirationRate = -2.83E-6),
+    list(compoundName = "Aut", molarRatios = c(C=1, N=16/106), initialMols = 0, respirationRate = -2.83E-6),
+    list(compoundName = "Met", molarRatios = c(C=1, N=16/106), initialMols = 0, respirationRate = -2.83E-6),
+    list(compoundName = "DOM", molarRatios = c(C=1, N=16/106), initialMols = 0),
     list(compoundName = "XOM", molarRatios = c(C=1, N=16/106), initialMols = 0),
-    list(compoundName = "DOM", molarRatios = c(C=1, N=16/106), initialMols = 1),
     list(compoundName = "CH4", molarRatios = c(C=1), initialMols = 0),
     list(compoundName = "NH4", molarRatios = c(N=1), initialMols = 0),
     list(compoundName = "NO3", molarRatios = c(N=1), initialMols = 0),
-    list(compoundName = "NO2", molarRatios = c(N=1), initialMols = 0),
-    list(compoundName = "N2O", molarRatios = c(N=1), initialMols = 0),
-    list(compoundName = "O2" , molarRatios = c(O=1), initialMols = 10),
+    #     list(compoundName = "NO2", molarRatios = c(N=1), initialMols = 0),
+    #     list(compoundName = "N2O", molarRatios = c(N=1), initialMols = 0),
+    list(compoundName = "O2" , molarRatios = c(O=1), initialMols = 0),
     list(compoundName = "SO4", molarRatios = c(S=1), initialMols = 0),
     list(compoundName = "CO2", molarRatios = c(C=1), initialMols = 0, sourceSink = T),
     list(compoundName = "N2" , molarRatios = c(N=1), initialMols = 0, sourceSink = T),
     list(compoundName = "HS" , molarRatios = c(S=1), initialMols = 0, sourceSink = T),
     list(compoundName = "Ox" , molarRatios = c(O=1), initialMols = 0, sourceSink = T)
   )
+  compounds <<- unlist(lapply(compoundParams, do.call, what = "compoundFactory"), recursive = F)
+}
 
-  compounds = unlist(lapply(compoundParams, do.call, what = "compoundFactory"), recursive = F)
-
+gangstaProcesses = function() {
   processParams = list(
     list(
       name = "AssimDOM",
@@ -73,14 +55,14 @@ gangstaTest = function() {
       molarTerms = list(N = 1),
       organismName = c("Het", "Aut", "Met")
     ),
-    list(
-      name = "AssimNO2",
-      energyTerm = -1.25E-04,
-      fromCompoundNames = list(N = "NO2"),
-      toCompoundNames = list(N = "."),
-      molarTerms = list(N = 1),
-      organismName = c("Het", "Aut", "Met")
-    ),
+#     list(
+#       name = "AssimNO2",
+#       energyTerm = -1.25E-04,
+#       fromCompoundNames = list(N = "NO2"),
+#       toCompoundNames = list(N = "."),
+#       molarTerms = list(N = 1),
+#       organismName = c("Het", "Aut", "Met")
+#     ),
     list(
       name = "AssimNH4",
       energyTerm = -3.18E-05,
@@ -100,32 +82,15 @@ gangstaTest = function() {
       processSuffix = c("ofHet", "ofDOM")
     ),
     list(
-      name = "DenitStep1",
-      energyTerm = 2.88E-04,
+      name = "Denit",
+      energyTerm = 2.88E-4 + 4.15E-4 + 6.45E-4,
       fromCompoundNames = list(C = c(".", "DOM"), N = c(".", "DOM"), N = "NO3"),
-      toCompoundNames = list(C = "CO2", N = "NH4", N = "NO2"),
-      molarTerms = list(C = 1, N = NA, N = 2),
-      organismName = "Het",
-      processSuffix = c("ofHet", "ofDOM")
-    ),
-    list(
-      name = "DenitStep2",
-      energyTerm = 4.15E-04,
-      fromCompoundNames = list(C = c(".", "DOM"), N = c(".", "DOM"), N = "NO2"),
-      toCompoundNames = list(C = "CO2", N = "NH4", N = "N2O"),
-      molarTerms = list(C = 1, N = NA, N = 2),
-      organismName = "Het",
-      processSuffix = c("ofHet", "ofDOM")
-    ),
-    list(
-      name = "DenitStep3",
-      energyTerm = 6.45E-04,
-      fromCompoundNames = list(C = c(".", "DOM"), N = c(".", "DOM"), N = "N2O"),
       toCompoundNames = list(C = "CO2", N = "NH4", N = "N2"),
-      molarTerms = list(C = 1, N = NA, N = 4),
+      molarTerms = list(C = 3, N = NA, N = 8),
       organismName = "Het",
       processSuffix = c("ofHet", "ofDOM")
     ),
+
     list(
       name = "SulfateRed",
       energyTerm = 3.8E-05,
@@ -145,19 +110,11 @@ gangstaTest = function() {
       processSuffix = c("ofHet", "ofDOM")
     ),
     list(
-      name = "NitrifStep1",
-      energyTerm = 1.83E-04,
+      name = "Nitrif",
+      energyTerm = 1.83E-04 + 1.48E-04,
       fromCompoundNames = list(N = "NH4", O = "O2"),
-      toCompoundNames = list(N = "NO2", O = "Ox"),
-      molarTerms = list(N = 2/3, O = 2),
-      organismName = "Aut"
-    ),
-    list(
-      name = "NitrifStep2",
-      energyTerm = 1.48E-04,
-      fromCompoundNames = list(N = "NO2", O = "O2"),
       toCompoundNames = list(N = "NO3", O = "Ox"),
-      molarTerms = list(N = 2, O = 2),
+      molarTerms = list(N = 8/3, O = 4),
       organismName = "Aut"
     ),
     list(
@@ -176,6 +133,49 @@ gangstaTest = function() {
       molarTerms = list(C = 1, N = NA),
       organismName = c("Aut", "Met")
     )
+#     list(
+#       name = "DenitStep1",
+#       energyTerm = 2.88E-04,
+#       fromCompoundNames = list(C = c(".", "DOM"), N = c(".", "DOM"), N = "NO3"),
+#       toCompoundNames = list(C = "CO2", N = "NH4", N = "NO2"),
+#       molarTerms = list(C = 1, N = NA, N = 2),
+#       organismName = "Het",
+#       processSuffix = c("ofHet", "ofDOM")
+#     ),
+#     list(
+#       name = "DenitStep2",
+#       energyTerm = 4.15E-04,
+#       fromCompoundNames = list(C = c(".", "DOM"), N = c(".", "DOM"), N = "NO2"),
+#       toCompoundNames = list(C = "CO2", N = "NH4", N = "N2O"),
+#       molarTerms = list(C = 1, N = NA, N = 2),
+#       organismName = "Het",
+#       processSuffix = c("ofHet", "ofDOM")
+#     ),
+#     list(
+#       name = "DenitStep3",
+#       energyTerm = 6.45E-04,
+#       fromCompoundNames = list(C = c(".", "DOM"), N = c(".", "DOM"), N = "N2O"),
+#       toCompoundNames = list(C = "CO2", N = "NH4", N = "N2"),
+#       molarTerms = list(C = 1, N = NA, N = 4),
+#       organismName = "Het",
+#       processSuffix = c("ofHet", "ofDOM")
+#     ),
+#     list(
+#       name = "NitrifStep1",
+#       energyTerm = 1.83E-04,
+#       fromCompoundNames = list(N = "NH4", O = "O2"),
+#       toCompoundNames = list(N = "NO2", O = "Ox"),
+#       molarTerms = list(N = 2/3, O = 2),
+#       organismName = "Aut"
+#     ),
+#     list(
+#       name = "NitrifStep2",
+#       energyTerm = 1.48E-04,
+#       fromCompoundNames = list(N = "NO2", O = "O2"),
+#       toCompoundNames = list(N = "NO3", O = "Ox"),
+#       molarTerms = list(N = 2, O = 2),
+#       organismName = "Aut"
+#     ),
 
   )
 
@@ -192,19 +192,136 @@ gangstaTest = function() {
 #   )
 
   expandedSpecs = unlist(lapply(processParams, expandMultiprocessSpec), recursive = F)
-  processes = unlist(lapply(expandedSpecs, function(x) do.call(processFactory, args = c(list(compounds), x))), recursive = F)
-  names(processes) = getGangstaAttribute(processes, gangstaAttributeName("name"))
+  processes <<- unlist(lapply(expandedSpecs, function(x) do.call(processFactory, args = c(list(compounds), x))), recursive = F)
+  names(processes) <<- getGangstaAttribute(processes, gangstaAttributeName("name"))
+}
+
+gangstaTest = function(){
+  gangstaCompounds()
+  gangstaProcesses()
   return(c(compounds, processes))
 }
 
-doItGangsta = function(simple = T, file = file.choose()){
-  if(simple) {
-    gangstas <<- simpleTest()
-  } else {
-    gangstas <<- gangstaTest()
+leakIn = function(){
+  leakInList = list(
+    list(
+      list(compoundName = "Het", additionalMols = 1.1),
+      list(compoundName = "Aut", additionalMols = 1.2),
+      list(compoundName = "Met", additionalMols = 1.3),
+      list(compoundName = "DOM", additionalMols = 1.4),
+#       list(compoundName = "XOM", additionalMols = 1.5),
+#       list(compoundName = "CH4", additionalMols = 1.6),
+#       list(compoundName = "NH4", additionalMols = 1.7),
+#       list(compoundName = "NO3", additionalMols = 1.8),
+#       #     list(compoundName = "NO2", additionalMols = 0),
+#       #     list(compoundName = "N2O", additionalMols = 0),
+#       list(compoundName = "O2" , additionalMols = 1.9),
+#       list(compoundName = "SO4", additionalMols = 2.0),
+#       list(compoundName = "CO2", additionalMols = 2.1),
+#       list(compoundName = "N2" , additionalMols = 2.2),
+#       list(compoundName = "HS" , additionalMols = 2.3),
+      list(compoundName = "Ox" , additionalMols = 2.4)
+    ),
+    list(
+      list(compoundName = "Het", additionalMols = 0.1),
+      list(compoundName = "Aut", additionalMols = 0.2),
+#       list(compoundName = "Met", additionalMols = 0.3),
+#       list(compoundName = "DOM", additionalMols = 0.4),
+#       list(compoundName = "XOM", additionalMols = 0.5),
+#       list(compoundName = "CH4", additionalMols = 0.6),
+#       list(compoundName = "NH4", additionalMols = 0.7),
+#       list(compoundName = "NO3", additionalMols = 0.8),
+#       #     list(compoundName = "NO2", additionalMols = 0),
+#       #     list(compoundName = "N2O", additionalMols = 0),
+#       list(compoundName = "O2" , additionalMols = 0.9),
+#       list(compoundName = "SO4", additionalMols = 1.0),
+#       list(compoundName = "CO2", additionalMols = 1.1),
+#       list(compoundName = "N2" , additionalMols = 1.2),
+#       list(compoundName = "HS" , additionalMols = 1.3),
+      list(compoundName = "Ox" , additionalMols = 1.4)
+    )
+  )
+}
 
+iterateGansta = function(gangstaObjects, lpObject, leakInList = leakIn(), omitInitialRun = TRUE){
+  startSuffix = gangstaVarName("startSuffix")
+  gangstaResultsList = list()
+  nameAttrName = gangstaAttributeName("name")
+  numberOfSteps = length(leakInList) + 1
+
+  gangstaCmpds = subsetGangstas(gangstas,"class", "compound")
+  gangstaCmpdNames = names(gangstaCmpds)
+  gangstaPools = subsetGangstas(gangstas,"class", "pool")
+  gangstaPoolNames = names(gangstaPools)
+
+  listLoc = 0
+
+  for(i in 1:numberOfSteps) {
+
+    if(!omitInitialRun || i > 1) {
+      lpStatus = suppressWarnings(lpSolveAPI::solve.lpExtPtr(lpObject))
+      pd = poolDifs(gangstaObjects, lpObject, simple = FALSE)
+      mt = massTransfers(gangstaObjects, lpObject, simple = FALSE, byProcess = FALSE)
+
+      stepOutput = list(objective = lpSolveAPI::get.objective(lpObject), status = lpStatus, poolVals = pd, massTransferVals = mt)
+      listLoc = listLoc + 1
+      gangstaResultsList[[listLoc]] = stepOutput
+      names(gangstaResultsList)[listLoc] = paste0("Iteration_", listLoc)
+
+      if(i == numberOfSteps) break
+    }
+
+    stepLeakInList = unlist(leakInList[i], recursive = FALSE)
+    numberOfLeakIns = length(stepLeakInList)
+    cmpdNames = mapply("[[", stepLeakInList, 1, SIMPLIFY = FALSE)
+    refPoolMolsToAdd = unlist(mapply("[[", stepLeakInList, 2, SIMPLIFY = FALSE))
+
+    cmpds = gangstaCmpds[gangstaCmpdNames %in% cmpdNames]
+    pools = unlist(lapply(cmpdNames, function(c) subsetGangstas(gangstaPools, "compoundName", c)), recursive = FALSE)
+    poolNames = names(pools)
+
+    refPoolNames = unlist(lapply(cmpdNames, function(c) getGangstaAttribute(getGangstas(gangstas, c), "referencePoolName")))
+    names(refPoolMolsToAdd) = refPoolNames
+
+    stoichPoolNames = poolNames[!(!is.na(match(poolNames, refPoolNames)))]
+    stoichMultipliers = getGangstaAttribute(pools[!is.na(match(poolNames, stoichPoolNames))], "molarRatio")
+    stoichPools = pools[!is.na(match(poolNames, stoichPoolNames))]
+    stoichPoolCompoundNames = getGangstaAttribute(stoichPools, "compoundName")
+    stoichPoolCompounds = cmpds[!is.na(match(cmpdNames, stoichPoolCompoundNames))]
+    stoichPoolReferencePoolNames = getGangstaAttribute(stoichPoolCompounds, "referencePoolName")
+    refPoolMolsToMultiplyToStoichPoolMols = unlist(refPoolMolsToAdd[!is.na(match(refPoolNames, stoichPoolReferencePoolNames))])
+    stoichPoolMolsToAdd = stoichMultipliers * refPoolMolsToMultiplyToStoichPoolMols
+
+    molsToAdd = c(refPoolMolsToAdd, stoichPoolMolsToAdd)
+    if(!omitInitialRun || i>1) {
+      molIndices = match(names(molsToAdd), row.names(pd))
+      allMolsToAdd = molsToAdd + pd$final[molIndices]
+    } else {
+      allMolsToAdd = molsToAdd
+    }
+
+    names(allMolsToAdd) = paste(names(allMolsToAdd), startSuffix, sep = ".")
+
+    ### update initial mols using previous values + leak in list
+    initPoolIndexes = match(names(allMolsToAdd), dimnames(lpObject)[[2]])
+    lpSolveAPI::set.bounds(lpObject, lower = allMolsToAdd, upper = allMolsToAdd, columns = initPoolIndexes)
   }
+  return(gangstaResultsList)
+}
+
+
+
+
+
+doItGangsta = function(lpNum, file = file.choose()){
+  gangstas <<- gangstaTest()
   equations <<- makeEquations(gangstas)
   writeGangstaModel(equations, file)
-  test.lp <<- readGangsta.lp(file)
+  gangsta.lp = readGangsta.lp(file)
+  assign(paste0("test.lp.0", lpNum), gangsta.lp, envir = .GlobalEnv)
+  # test.lp <<- readGangsta.lp(file)
+  return(get(paste0("test.lp.0", lpNum)))
 }
+
+
+
