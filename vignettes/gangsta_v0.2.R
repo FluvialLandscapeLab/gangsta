3a546cba8ea4f0d5ddcf48f00f53070759cc13be
Ann Marie Reinhold <AMReinhold@users.noreply.github.com>
2018-01-12 11:46:23 -0700

    Corrected an error in the parameterization in the vignette code specifying the assimilatory processes.

--- vignettes/gangsta_v0.2.R
+++ vignettes/gangsta_v0.2.R
@@ -109,7 +109,7 @@ myGangstas =
       energyTerm = -3.5E-03,
       fromCompoundNames = list(C = "CO2", O = "CO2", O = "CO2"),
       toCompoundNames = list(C = "Aut", O = "Aut", O = "Ox"),
-      molarTerms = list(C = 1, O = EcoStoichO, O = 2 - EcoStoichO),
+      molarTerms = list(C = 1, O = 2, O = 2),
       organismName = "Aut"
     ),
     processFactory(
@@ -127,7 +127,7 @@ myGangstas =
       energyTerm = -1.55E-04,
       fromCompoundNames = list(N = "NO3", O = "NO3", O = "NO3"),
       toCompoundNames = list(N = "Aut", O = "Aut", O= "Ox"),
-      molarTerms = list(N = 1, O = EcoStoichO, O = 3 - EcoStoichO),
+      molarTerms = list(N = 1, O = 3, O = 3),
       organismName = "Aut"
     ),
     processFactory(
@@ -136,7 +136,7 @@ myGangstas =
       energyTerm = -1.55E-04,
       fromCompoundNames = list(N = "NO3", O = "NO3", O = "NO3"),
       toCompoundNames = list(N = "Met", O = "Met", O= "Ox"),
-      molarTerms = list(N = 1, O = EcoStoichO, O = 3 - EcoStoichO),
+      molarTerms = list(N = 1, O = 3, O = 3),
       organismName = "Met"
     ),
     processFactory(
