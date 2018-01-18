a921de7bf35d4b99e77fc2dc278093b416df10e9
Ann Marie Reinhold <AMReinhold@users.noreply.github.com>
2018-01-18 10:39:52 -0700

    Updating documentation.

--- R/gangsta_s3Objects.R
+++ R/gangsta_s3Objects.R
@@ -48,20 +48,20 @@
 #' \code{respirationRate} is energy (J, KJ, etc.) per mol (or umol, etc.) of
 #' \code{compound} per timestep length.
 #'
-#' \code{pool}s have attributes called \code{name}, \code{elementName} which is
+#' \code{pool} objects have attributes called \code{name}, \code{elementName} which is
 #' the chemical element stored in the \code{pool}, \code{compoundName} which
 #' indicates the compound with which the pool is associated, and
 #' \code{molarRatio} which describes the ratio of atoms in the \code{pool} per
 #' unit molecule, i.e., \code{compound}.
 #'
-#' \code{process}es have attributes called \code{name}, \code{energyTerm} which
+#' \code{process} objects have attributes called \code{name}, \code{energyTerm} which
 #' is equal to the energy generated or consumed by the \code{process},
 #' \code{organismName} which is the name of the \code{organism} carrying out the
 #' \code{process}, and \code{transferOptions} which are indices for the
 #' \code{transfer}s associated with each \code{process}.  Each \code{process} with
 #' nonzero \code{energyTerm} is of class "metabolic."
 #'
-#' \code{transfer}s have attributes called \code{name}, \code{from} which is the
+#' \code{transfer} objects have attributes called \code{name}, \code{from} which is the
 #' \code{pool} from which atoms are being transfered, \code{to} which is the
 #' \code{pool} to which atoms are being transfered, \code{molarTerm} which
 #' enforces the stoichiometry of the \code{process}, \code{molarAffinity} which
@@ -126,8 +126,8 @@
 #'   \code{processFactory} will create these automatically.
 #'   \code{transferOptions} consist of a list of integer or numeric vectors
 #'   containing the indicies of transfers in a process.  This specification is
-#'   appropriate when each \code{from pool} has exactly one code \code {to
-#'   pool}.  However, these must be specified when more than one \code {to pool}
+#'   appropriate when each \code{from pool} has exactly one \code{to
+#'   pool}.  However, these must be specified when more than one \code{to pool}
 #'   exists for any transfer involved in a process, i.e., indicies are grouped
 #'   when transfers represent optional pathways. For instance, if a process has
 #'   four transfers (fromA -> toA, fromB -> toB1, fromB -> toB2, fromC -> toC),
