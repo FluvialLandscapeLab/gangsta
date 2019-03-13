ff2ff6209661fd2ec24bd3f271ea2232cf4152ce
Elizabeth Mohr <elizabethjmohr@gmail.com>
2019-03-13 11:16:27 -0600

    Added an element constructor and started writing a function to "enable" isotopes

--- R/gangsta_s3Objects.R
+++ R/gangsta_s3Objects.R
@@ -1,3 +1,9 @@
+# Attribute values of the \code{gangsta} S3 objects are accessible with
+# the notation x$name. The constructors \code{compound}, \code{pool},
+# \code{process}, and \code{transfer} can be called individually, but this is
+# discouraged since the factory functions do substantial error checking and
+# assure that references between the \code{gangsta} objects are correct.
+
 #' Create \code{gangsta} objects
 #'
 #' \code{compoundFactory} and \code{processFactory} are the primary functions
@@ -11,22 +17,16 @@
 #' named lists with the class arribute set.  The list's names are used as
 #' attribute names and the values in the lists are the attribute values.
 #'
-# Thus, attribute values of the \code{gangsta} S3 objects are accessible with
-# the notation x$name. The constructors \code{compound}, \code{pool},
-# \code{process}, and \code{transfer} can be called individually, but this is
-# discouraged since the factory functions do substantial error checking and
-# assure that references between the \code{gangsta} objects are correct.
-#
 #' \code{compound} objects represent chemical species (e.g., SO4 and HS) that
 #' generally contain one or more chemical elements; the mols (or umols, etc.) of
 #' each element of interest (i.e., tracked element) within a \code{compound} are
 #' tracked using a \code{pool}.  Each \code{pool} records the mols of a tracked
 #' element in the \code{compound}.
 #'
-#' The \code{molarRatios} describe the chemical composition of the
-#' tracked elements contained in compounds. For example, the \code{molarRatios}
-#' for water could be represented as \code{c(H = 2, O = 1)} because a molecule
-#' of water is composed of two hydrogen atoms and one oxygen atom.
+#' The \code{molarRatios} describe the chemical composition of the tracked
+#' elements contained in compounds. For example, the \code{molarRatios} for
+#' water could be represented as \code{c(H = 2, O = 1)} because a molecule of
+#' water is composed of two hydrogen atoms and one oxygen atom.
 #'
 #' Not all elements in a \code{compound} must be tracked.  The model developer
 #' only creates \code{pool}s for the elements of interest.  For instance, when
@@ -47,27 +47,28 @@
 #' \code{respirationRate} is energy (J, KJ, etc.) per mol (or umol, etc.) of
 #' \code{compound} per timestep.
 #'
-#' \code{pool} objects have attributes called \code{name}, \code{elementName} which is
-#' the chemical element stored in the \code{pool}, \code{compoundName} which
-#' indicates the compound with which the pool is associated, and
+#' \code{pool} objects have attributes called \code{name}, \code{elementName}
+#' which is the chemical element stored in the \code{pool}, \code{compoundName}
+#' which indicates the compound with which the pool is associated, and
 #' \code{molarRatio} which describes the ratio of atoms in the \code{pool} per
 #' unit molecule, i.e., \code{compound}.
 #'
-#' \code{process} objects have attributes called \code{name}, \code{energyTerm} which
-#' is equal to the energy generated or consumed by the \code{process},
+#' \code{process} objects have attributes called \code{name}, \code{energyTerm}
+#' which is equal to the energy generated or consumed by the \code{process},
 #' \code{organismName} which is the name of the \code{organism} carrying out the
 #' \code{process}, and \code{transferOptions} which are indices for the
-#' \code{transfer}s associated with each \code{process}.  Each \code{process} with
-#' nonzero \code{energyTerm} is of class "metabolic."
+#' \code{transfer}s associated with each \code{process}.  Each \code{process}
+#' with nonzero \code{energyTerm} is of class "metabolic."
 #'
-#' \code{transfer} objects have attributes called \code{name}, \code{from} which is the
-#' \code{pool} from which atoms are being transfered, \code{to} which is the
-#' \code{pool} to which atoms are being transfered, \code{molarTerm} which
+#' \code{transfer} objects have attributes called \code{name}, \code{from} which
+#' is the \code{pool} from which atoms are being transfered, \code{to} which is
+#' the \code{pool} to which atoms are being transfered, \code{molarTerm} which
 #' enforces the stoichiometry of the \code{process}, \code{molarAffinity} which
 #' is equal to the \code{energyTerm} of the \code{process} divided by the
 #' \code{molarTerm} of the \code{transfer}, \code{processName} which is the name
 #' of the \code{process} that the \code{transfer} is associated with, and
-#' \code{limitToInitMolecules} which is a Boolean that inherits from \code{process}.
+#' \code{limitToInitMolecules} which is a Boolean that inherits from
+#' \code{process}.
 #'
 #' \code{gangsta}-derived models can operate using any unit of atomic count unit
 #' (mols, umols, etc.), unit of energy (Joules, KJ, etc.) over any time unit
@@ -78,10 +79,7 @@
 #' functions in the \code{gangsta} package.
 #'
 #' @param compoundName A character vector of \code{length = 1} containing the
-#'   name of the
-#' \code{compound} to be created.
-# (or for\code{pool}, the name of the \code{compound} to which the \code{pool}
-#   belongs).
+#'   name of the \code{compound} to be created.
 #' @param molarRatios A named numeric vector.  Vector names are the names of the
 #'   chemical elements (think 'periodic table in chemistry') that are in the
 #'   \code{compound} and that are to be tracked by \code{gangsta}.  Values in
@@ -89,16 +87,17 @@
 #'   \code{compound}.
 #' @param initialMolecules The number of mols (or umols, etc.) of the compound
 #'   available at the beginning of the simulation.  If the \code{compound} is of
-#'   type \code{infiniteCompound}, then \code{initialMolecules} must be set to 0.
+#'   type \code{infiniteCompound}, then \code{initialMolecules} must be set to
+#'   0.
 #' @param respirationRate The respiration rate (in units of energy per unit of
 #'   biomass per model timestep); applies to \code{organism} objects.  When
 #'   \code{respirationRate} is numeric, an \code{organism} object is returned.
 #'   When NA, a \code{compound} object is returned.  \code{respirationRate}
-#' values must be negative (i.e., energy cost to the organisms).
+#'   values must be negative (i.e., energy cost to the organisms).
 #' @param infiniteCompound Boolean when set to \code{TRUE} tags a
 #'   \code{compound} as being unlimited in supply. \code{infiniteCompound}s
-#' represent sources/sinks.  When \code{infiniteCompound} is set to \code{TRUE},
-#' \code{initialMolecules} must be set to 0.
+#'   represent sources/sinks.  When \code{infiniteCompound} is set to
+#'   \code{TRUE}, \code{initialMolecules} must be set to 0.
 #' @param gangstaObjects A list of objects of class \code{gangsta} representing
 #'   the \code{organisms}, \code{compounds}, \code{pools}, \code{processes}, and
 #'   \code{transfers} to be included in the model.  These objects are created
@@ -130,28 +129,25 @@
 #'   \code{processFactory} will create these automatically.
 #'   \code{transferOptions} consist of a list of integer or numeric vectors
 #'   containing the indicies of transfers in a process.  This specification is
-#'   appropriate when each \code{from pool} has exactly one \code{to
-#'   pool}.  However, these must be specified when more than one \code{to pool}
-#'   exists for any transfer involved in a process, i.e., indicies are grouped
-#'   when transfers represent optional pathways. For instance, if a process has
-#'   four transfers (fromA -> toA, fromB -> toB1, fromB -> toB2, fromC -> toC),
-#'   the second and third transfers can represent an option.  fromB can go to
-#'   either toB1 or toB2, so long as the sum of the two options is in
-#'   stoichiometric balance with the A and C tranfers.  To represent such an
-#'   option, the transferOption list would be \code{list(1, 2:3, 4)}.
+#'   appropriate when each \code{from pool} has exactly one \code{to pool}.
+#'   However, these must be specified when more than one \code{to pool} exists
+#'   for any transfer involved in a process, i.e., indicies are grouped when
+#'   transfers represent optional pathways. For instance, if a process has four
+#'   transfers (fromA -> toA, fromB -> toB1, fromB -> toB2, fromC -> toC), the
+#'   second and third transfers can represent an option.  fromB can go to either
+#'   toB1 or toB2, so long as the sum of the two options is in stoichiometric
+#'   balance with the A and C tranfers.  To represent such an option, the
+#'   transferOption list would be \code{list(1, 2:3, 4)}.
 #' @param organismName Name of the organism carrying out the \code{process}.
-
-# @param elementName The name of the element contained by the created
-#   \code{pool}.
-# @param molarRatio The ratio of the elemental mol in a \code{bound pool} to the
-#   mol in its reference \code{pool}.  When molarRatio is NA, a \code{pool}
-#   object is return.  When molarRatio is numeric, a \code{bound pool} object of
-#   returned.
-#' @return \code{compoundFactory} returns a list of \code{compound} and \code{pool}
-#'   objects. \code{processFactory} return a list of \code{process} and
-#'   \code{transfer} objects.
-# The remaining constructor methods return an individual \code{gangsta} object of the
-# class corresponding to the function name.
+#' @param elementList A named list of character vectors where each vector
+#'   contains the names of the isotopes to be tracked for a single element.
+#'   Typically, these are character representations of each isotope's molecular
+#'   weight. The name of each element in this list corresponds to the element
+#'   names used in the \code{molarRatios} list.
+#'
+#' @return \code{compoundFactory} returns a list of \code{compound} and
+#'   \code{pool} objects. \code{processFactory} return a list of \code{process}
+#'   and \code{transfer} objects.
 #' @export
 compoundFactory = function(compoundName, molarRatios, initialMolecules, respirationRate = NA, infiniteCompound = F) {
   checkNames = unique(names(molarRatios))==""
@@ -166,6 +162,14 @@ compoundFactory = function(compoundName, molarRatios, initialMolecules, respirat
   newCompound = list(compound(compoundName, initialMolecules, respirationRate, infiniteCompound))
   names(newCompound) = compoundName
   return(c(newCompound, newPools))
+}
+
+#' @rdname compoundFactory
+#' @export
+enableIsotopeTracking = function(gangstaObjects, elementList){
+  poolObjectIdx = subsetGanstas(gangstaObjects, "class", getOption("gangsta.classes")["pool"], asIndex = T)
+  elementNames = names(elementList)
+
 }
 
 #' @rdname compoundFactory
@@ -241,11 +245,21 @@ compound = function(compoundName, initialMolecules, respirationRate = NA, infini
 }
 
 # @rdname compoundFactory
+# @param elementName The name of the element contained by the created
+#   \code{pool}.
 pool = function(compoundName, elementName, molarRatio) {
   poolName = makePoolNames(compoundName, elementName)
   newPool = list(name = poolName, elementName = elementName, compoundName = compoundName, molarRatio = molarRatio)
   class(newPool) = c("pool", "gangsta")
   return(newPool)
+}
+
+# @rdname compoundFactory
+element = function(elementName, isotopeNames){
+  newElement = list(name = elementName,
+                    isotopes = isotopeNames)
+  class(newElement) = c("element", "gangsta")
+  return(newElement)
 }
 
 # @rdname compoundFactory
